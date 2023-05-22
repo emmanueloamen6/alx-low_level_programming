e3c576f53ab129e803de381121b84142f2cad8ec
emmanueloamen6 <emmanueloamen6@gmail>
2023-05-22 08:02:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
- * main - The program prints the size of various variable types
- * on the computer it is compiled and run on
- *
+ * main - Entry point
+ * Description: The program prints the size of various variable types
  *Return: 0 Always (success)
  */
 int main(void)
 {
-	int A = sizeof(char);
-	int B = sizeof(int);
-	int C = sizeof(long int);
-	int D = sizeof(long long int);
-	int E = sizeof(float);
+	int O = sizeof(char);
+	int P = sizeof(int);
+	int Q = sizeof(long int);
+	int R = sizeof(long long int);
+	int X = sizeof(float);
 
-	printf("Size of a char: %c byte(s)\n", (char)A);
-	printf("Size of an int: %d byte(s)\n", (int)B);
-	printf("Size of a long int: %d byte(s)\n", (int)C);
-	printf("Size of a long long int: %d bytes(s)\n", (int)D);
-	printf("Size of a float: %f bytes(s)\n", (int)E);
+	printf("Size of a char: %d byte(s)\n", (int)O);
+	printf("Size of ani int: %d byte(s)\n", (int)P);
+	printf("Size of a long int: %d byte(s)\n", (int)Q);
+	printf("Size of a long long int: %d bytes(s)\n", (int)R);
+	printf("Size of a float: %d bytes(s)\n", (int)X);
 	return (0);
 }
