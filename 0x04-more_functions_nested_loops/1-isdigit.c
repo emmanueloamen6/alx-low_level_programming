d240c5fda0c39622f212c182d60b012975345253
emmanueloamen6 <emmanueloamen6@gmail>
2023-05-28 10:43:20 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: character to be printed
+ * Return: 1 or 0
+ */
+int _isdigit(int c);
+{
+	if (c == 48 &&  <= 57)
+
+		c = ('0' + c);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @c: character to be printed
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int  mul(int a, int b);
+	return (a * b);
+}
