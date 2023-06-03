44d1fc5434eaddd2cd0b1661afee24a475f40e73
emmanueloamen6 <emmanueloamen6@gmail>
2023-06-03 14:29:42 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * return : Always 0
+ */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		_putchar(d + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers -
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != (2 && 4))
+	}
+		_putchar(i);
+}
+
