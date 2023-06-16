acca98495f16bbd5d8e6eda41d7bc6b6af1102e1
emmanueloamen6 <emmanueloamen6@gmail>
2023-06-16 16:18:32 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,50 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat -
- * Return:
+ * string_nconcat - concatenates two strings.
+ * @s1: pointer to the string
+ * @s2: pointer to the string
+ * @n: number of bytes
+ * Return: newly allocated space in memory concat
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int len = n, index;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
+	while (s1 && s1[len1])
 	{
-		s1 = "";
-	if (s2 == NULL)
-	s2 = "";
-	for (index = 0; s1[index]; index++)
-		len++;
-	concat = malloc(sizeof(char) * (len + 1));
-	if (concat == NULL)
-		return (NULL);
-	len = 0;
-	for (index = 0; s1[index]; index++)
-	concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-	concat[len++] = s2[index];
-	concat[len] = '\0';
-	free(s2);
+		len1++;
 	}
+	while (s1 && s1[len1])
+	{
+		len2++;
+	}
+	if (n < len2)
+	{
+		concat = malloc(sizeof(char) * (len1 + n + 1));
+	}
+	else
+	{
+		concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	}
+	if (!concat)
+	{
+		return (NULL);
+	}
+	while (i < len1)
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (n < len2 && i < (len1 + n))
+	{
+		concat[i++] = s2[j++];
+	}
+	while (n >= len2 && i < (len1 + len2))
+	{
+		concat[i++] = s2[j++];
+	}
+	concat[i] = '\0';
 	return (concat);
 }
