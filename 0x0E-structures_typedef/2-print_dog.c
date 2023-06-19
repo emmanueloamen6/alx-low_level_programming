d05cb97398b9a8672eb3e3678ed83038dc1bbfcb
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-06-19 16:14:41 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,6 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->age < 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
 	if (d->name == NULL)
 	{
 		printf("Name: NULL\n");
@@ -28,6 +20,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
+	if (d->age < 0)
+	{
+		printf("Age: (nil\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
 	{
 		printf("owner: NULL\n");
@@ -35,6 +35,7 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("owner: %s\n", d->owner);
+
 	}
 }
 
