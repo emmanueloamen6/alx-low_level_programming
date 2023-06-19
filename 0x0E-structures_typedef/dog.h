ecd85cd20dd680e2bd782966352b87cbb23f19e1
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-06-19 12:50:07 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: memeber 1
+ * @age: member 2
+ * @owner: member 3
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
