6eba59c2525e5d01e69bbb3cf89a291997128d50
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-07-11 14:45:20 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,46 @@
 #include "main.h"
+
 /**
- * create_file - creates a file.
- * @filename: name of the file to be created
- * @text_content: content of text to be written in it
- * Return: -1 if filename is NULL
- *  or on failure
- * 1 on success,
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, file;
-	ssize_t w;
+	ssize_t w = 0;
+	int file;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == 0)
+		return (-1);
+	if (text_content == NULL)
 		text_content = " ";
-	for (i = 0; text_content[i] != '\0'; i++)
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
-
-		return (-1);
+	w = write(file, text_content, _strlen(text_content));
 	close(file);
-	w = write(file, text_content, i);
-	if (w < 0)
+	if (w == -1)
 		return (-1);
 	return (1);
 }
