b6c65f4afe61c395de3554b117f4d3441729c9f9
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-07-11 14:49:31 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,13 +34,12 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == 0)
+	if (file < 0)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
-	w = write(file, text_content, _strlen(text_content));
+		w = write(file, text_content, _strlen(text_content));
 	close(file);
-	if (w == -1)
+	if (w < 0)
 		return (-1);
 	return (1);
 }
