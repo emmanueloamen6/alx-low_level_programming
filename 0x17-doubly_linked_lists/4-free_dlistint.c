f1257432759910d78001f0a96b947b9f0333f7cf
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-22 03:50:57 -0700

    doubly linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * free_dlistint: frees a dlistint_t list.
+ * free_dlistint - frees a dlistint_t list.
  * @head: free a dlistint_t list.
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t* next_node;
-	dlistint_t* actual_node;
+	dlistint_t *next_node;
+	dlistint_t *actual_node;
 
 	if (head)
 	{
