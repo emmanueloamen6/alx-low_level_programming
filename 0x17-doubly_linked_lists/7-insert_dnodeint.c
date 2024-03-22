2caf70f2f0af4e4e454c8624b9bc3dca0cac81fc
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-22 07:33:24 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 /**
- *
+ * * insert_dnodeint_at_index - inserts a node at a certain index
+  * of doubly linked list
+  * @h: double pointer to first node in list
+  * @idx: index to add new node at
+  * @n: value of new node
+  * Return: address to new node or NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int x;
 	dlistint_t *after, *before, *new_node;
 
 	if (!h)
@@ -28,12 +33,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		after = after->next;
 	else
 		return (NULL);
-	for (i = 1; after != NULL && i < idx; i++)
+	for (x = 1; after != NULL && x < idx; i++)
 	{
 		before = before->next;
 		after = after->next;
 	}
-	if (i == idx)
+	if (x == idx)
 	{
 		before->next = new_node;
 		new_node->prev = before;
