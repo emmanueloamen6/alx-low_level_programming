b0e76cadcfcae902a36877ffa17462aeb1e5352b
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-03-29 11:36:09 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/all-func.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int sub(int a, int b)
+{
+        return a - b;
+}
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int mul(int a, int b)
+{
+        return a * b;
+}
+
+/**
+ * div - division two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int div(int a, int b)
+{
+        return a / b;
+}
+
+/**
+ * mod - modulus two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int mod(int a, int b)
+{
+        return a % b;
+}
