bd6066bef528a371c0f35ab0f7d1357bd5143e72
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-06-04 09:05:39 -0700

    Hash Table

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,7 +12,7 @@ def island_perimeter(grid):
     for i in range(height):
         for j in range(width):
             if (grid[i][j] == 1):
-                size += 1
+                size += 1 
             if (j > 0 and grid[i][j - 1] == 1):
                 edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
